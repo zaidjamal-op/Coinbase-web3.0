06ab5bbbf957e3522b7ff38a4cdba95dd8fe751d
zaidjamal-op <iamzaidbinarshad@gmail.com>
2022-02-01 12:53:57 +0530

    fixing

--- components/Main.js
+++ components/Main.js
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom"
+import { modalState } from "../atoms/modalAtom";
+import { useWeb3 } from "@3rdweb/hooks"
 
 
 const Main = ({walletAddress, sanityTokens, thirdWebTokens}) => {
+  const { connectWallet } = useWeb3();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
   const router = useRouter();
   const [walletBalance, setWalletBalance] = useState(0)
@@ -60,10 +62,17 @@ const Main = ({walletAddress, sanityTokens, thirdWebTokens}) => {
       <div className="flex items-center justify-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-[#282b2f]">
       <h2 className="text-4xl mr-10 xl:text-5xl font-bold text-white">Assets</h2>
       <div className="flex items-center justify-center space-x-4">
-        <div className="border-2 border-gray-700 rounded-sm p-1 cursor-pointer">
-            <h4 className="text-[#26ad75]">Wallet Connected</h4>
-            <div className="text-white">{walletAddress.slice(0,7)} ... {walletAddress.slice(35)}</div>
-        </div>
+        {walletAddress ? (
+          <div className="border-2 border-gray-700 rounded-sm p-1 cursor-pointer">
+          <h4 className="text-[#26ad75]">Wallet Connected</h4>
+          <div className="text-white">{walletAddress.slice(0,7)} ... {walletAddress.slice(35)}</div>
+      </div>
+        ) : (
+          <button onClick={() => connectWallet('injected')} className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+          <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#3773f5] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Connect Wallet</span>
+      </button>
+        )}
       <button className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
       <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#3773f5] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
       <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white font-bold">Buy / Sell</span>
